31e85306a59cf38b72545ee04641e22354dec79d
woogi123 <07491@naver.com>
2025-05-06 22:27:00 +0900

    '랜선자르기_1654'

---
+++ 검색 알고리즘/랜선자르기_1654.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.read
+lines = input().strip().splitlines()
+
+K, N = map(int, lines[0].split())
+
+numbers = [int(lines[i+1]) for i in range(K)]
+
+front = 1
+rear = 2**31 - 1
+while True:
+    if front >= rear:
+        print(front)
+        break
+    mid = (front+rear+1) // 2
+    result = 0
+    for i in numbers:
+        num = i // mid
+        result+=num
+    
+    if result >= N:
+        front = mid
+    else:
+        rear = mid-1
