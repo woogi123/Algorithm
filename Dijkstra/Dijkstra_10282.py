d0b714058db29640f2c9edd7f425257bd51ace19
woogi123 <07491@naver.com>
2025-05-09 11:49:04 +0900

    'Dijkstra_10282'

--- Dijkstra/Dijkstra_10282.py
+++ Dijkstra/Dijkstra_10282.py
@@ -4,40 +4,45 @@ from heapq import heappush, heappop
 data = sys.stdin.read()
 lines = data.splitlines()
 
-N = int(lines[0])
-n, d, c = map(int, lines[1].split())
-
-graph = [[] for _ in range(n + 1)]
-
-for i in range(2, n + 1):
-    start, end, cost = map(int, lines[i].split())
-    graph[end].append((start, cost))
-
-INF = int(10**12)
-
 def dijkstra(c, graph):
-    lists = [INF] * (n + 1)
+        lists = [INF] * (n + 1)
 
-    heap = []
-    heappush(heap, (0, c))
+        heap = []
+        heappush(heap, (0, c))
 
-    while len(heap) != 0:
-        cost, node = heappop(heap)
-        if lists[node] > cost:
-            lists[node] = cost
-            for next_node, num in graph[node]:
-                if lists[next_node] > num + cost:
-                    heappush(heap, (num+cost, next_node))
+        while len(heap) != 0:
+            cost, node = heappop(heap)
+            if lists[node] > cost:
+                lists[node] = cost
+                for next_node, num in graph[node]:
+                    if lists[next_node] > num + cost:
+                        heappush(heap, (num + cost, next_node))
+        return lists
 
-    return lists
 
-lists = dijkstra(c, graph)
-count = 0
-max_num = 0
-for i in lists:
-    if i != INF:
-        if max_num < i:
-            max_num = i 
-        count+=1
+N = int(lines[0])
+
+num = 1
+for _ in range(N):
+    n, d, c = map(int, lines[num].split())
+    graph = [[] for _ in range(n + 1)]
+
+    for i in range(num + 1, num + d+1):
+        start, end, cost = map(int, lines[i].split())
+        graph[end].append((start, cost))
     
-print(count, max_num)
+    num += d + 1
+    INF = int(10**12)
+
+    lists = dijkstra(c, graph)
+    count = 0
+    max_num = 0
+    for i in lists:
+        if i != INF:
+            if max_num < i:
+                max_num = i 
+            count+=1
+        
+    print(count, max_num)
+
+# 코드가 굉장히 더럽지만, 그냥 다익스트라였다~
