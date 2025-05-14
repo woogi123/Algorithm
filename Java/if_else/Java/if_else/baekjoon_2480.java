7cea94c32bcad3331a662c1019a804878e518455
woogi123 <07491@naver.com>
2025-05-14 12:07:13 +0900

    'java_2480_Math_library'

--- Java/if_else/Java/if_else/baekjoon_2480.java
+++ Java/if_else/Java/if_else/baekjoon_2480.java
@@ -7,31 +7,17 @@ public class baekjoon_2480 {
         int y = scanner.nextInt();
         int z = scanner.nextInt();
 
-        if (x == y){
-            if (y == z){
-                System.out.printf("%d", 10000 + 1000 * x);
-            }else{
-                System.out.printf("%d", 1000 + 100*x);
-            }
-        }else{
-            if (y == z){
-                System.out.printf("%d", 1000 + 100*y);
-            }else{
-                if (x > y){
-                    if(x > z){
-                        System.out.printf("%d", 100*x);
-                    }else{
-                        System.out.printf("%d", 100*z);
-                    }
-                }else{
-                    if(y > z){
-                        System.out.printf("%d", 100*y);
-                    }else{
-                        System.out.printf("%d", 100*z);
-                    }
-                }
-                }
-            }
+        if (x == y && y == z) {
+            System.out.printf("%d", 10000 + 1000 * x);
+        } else if (x == y || x == z) {
+            System.out.printf("%d", 1000 + 100 * x);
+        } else if (y == z) {
+            System.out.printf("%d", 1000 + 100 * y);
+        } else {
+            int max = Math.max(x, Math.max(y, z));
+            System.out.printf("%d", 100 * max);
+        }
+        // Math 클래스 사용
 
         scanner.close();
     }
