400c43932c7520cf292ac2ab3656c5fa275489f8
woogi123 <07491@naver.com>
2025-05-14 12:32:20 +0900

    'java_25304'

--- Java/while_for/baekjoon_25304.java
+++ Java/while_for/baekjoon_25304.java
@@ -1,5 +1,22 @@
-package Java.if_else.while_for;
+import java.util.Scanner;
 
 public class baekjoon_25304 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        int result = 0; 
+        for (int i=0; i < N; i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            result = result + a * b;
+        }
+        if (X == result){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        scanner.close();
+    }
 }
