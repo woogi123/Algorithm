3a335dc120eced08bb773cc75f9b571bcda221cd
woogi123 <07491@naver.com>
2025-05-14 11:33:31 +0900

    'java_14681;

---
+++ Java/if_else/baekjoon_14681.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class baekjoon_14681 { // 여기서 백준은 Main으로 제출해야함
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("4");
+        }
+
+        scanner.close();
+    }
+}
