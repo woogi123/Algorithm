e9f3745f7d1e8b730f02f0bc90297db1b1c86b28
woogi123 <07491@naver.com>
2025-05-15 16:04:46 +0900

    'brute_force_19532'

---
+++ Java/brute_force/baekjoon_19532.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class baekjoon_19532 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int e = scanner.nextInt();
+        int f = scanner.nextInt();
+
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    System.out.printf("%d %d\n", x, y);
+                    return;
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
---
+++ Java/list/baekjoon_11728.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class baekjoon_11728 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            String a = scanner.nextLine();
+            System.out.println(a);
+        }
+        scanner.close();
+    }
+}
